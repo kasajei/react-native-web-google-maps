33257d060173a4f74e21a3254cd79a2f7f37f895
progfay <shun1856shun@gmail.com>
2020-01-21 13:18:44 +0900

    add search icon for Omnibox

--- src/Omnibox.js
+++ src/Omnibox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
   omnibox: {
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
     margin: '10px',
     backgroundColor: 'white',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     borderRadius: '4px',
     shadowColor: 'rgba(0, 0, 0, .2)',
@@ -16,15 +16,27 @@ const styles = StyleSheet.create({
       width: '0',
       height: '2px',
     },
+    flexDirection: 'row',
   },
   input: {
-    width: '400px',
+    width: '350px',
     height: '50px',
     borderWidth: 0,
     borderRadius: '4px',
     fontSize: '1rem',
-    paddingHorizontal: '10px',
     outline: 'none',
+    paddingHorizontal: '10px',
+  },
+  searchButton: {
+    width: 24,
+    height: 24,
+    overflow: 'hidden',
+    resizeMode: 'cover',
+    marginHorizontal: '10px',
+  },
+  searchIcon: {
+    width: 72,
+    height: 24,
   },
 });
 
@@ -68,6 +80,14 @@ class MapOmnibox extends Component {
             this.autocomplete.addListener('place_changed', this.onPlaceChanged);
           }}
         />
+        <TouchableOpacity style={styles.searchButton} onPress={this.onPlaceChanged}>
+          <Image
+            style={styles.searchIcon}
+            source={{
+              uri: 'https://maps.gstatic.com/tactile/omnibox/quantum_search_button-20150825-2x.png',
+            }}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
