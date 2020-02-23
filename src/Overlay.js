ed50a2256b247f17f40a9a5cd93c8dfe57ba4cf4
progfay <shun1856shun@gmail.com>
2020-02-23 21:31:54 +0900

    Overlay components only update on avaiable Google Maps API

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class MapOverlay extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return !this.overlay && nextProps.map && nextProps.maps;
+  }
+
   componentWillUnmount() {
     if (!this.overlay) return;
     this.overlay.setMap(null);
   }
 
-  componentWillUpdate() {
+  componentDidUpdate() {
     if (this.overlay || !this.props.map || !this.props.maps) return null;
     const [[north, west], [south, east]] = this.props.bounds;
     this.overlay = new this.props.maps.GroundOverlay(this.props.image.uri, {
