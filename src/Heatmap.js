d1536cdc0daa7168d6ea7538e64c36172b518761
progfay <shun1856shun@gmail.com>
2020-02-23 21:46:13 +0900

    support updating heatmap

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-class MapHeatmap extends React.Component {
+class MapHeatmap extends React.PureComponent {
   componentWillUnmount() {
     if (!this.heatmap) return;
     this.heatmap.setMap(null);
   }
 
-  componentWillUpdate() {
-    if (this.heatmap || !this.props.map || !this.props.maps) return null;
+  componentDidUpdate() {
+    console.log('update');
+    if (this.heatmap) {
+      this.heatmap.setOptions({
+        opacity: typeof this.props.opacity === 'number' ? this.props.opacity : 0.7,
+        radius: typeof this.props.radius === 'number' ? this.props.radius : 20,
+        data: this.props.points.map(({ latitude, longitude, weight = 1 }) => ({
+          location: new this.props.maps.LatLng(latitude, longitude),
+          weight,
+        })),
+      });
+      return;
+    }
+
+    if (!this.props.map || !this.props.maps) return;
     this.heatmap = new this.props.maps.visualization.HeatmapLayer({
       opacity: typeof this.props.opacity === 'number' ? this.props.opacity : 0.7,
       radius: typeof this.props.radius === 'number' ? this.props.radius : 20,
